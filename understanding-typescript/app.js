550ed1e75aee90a98c5cbf7e6bc80e6dfc9f6979
devin-lynch <devin.l.lynch@gmail.com>
2022-12-01 20:21:23 -0600

    Tuple/Enum

--- understanding-typescript/app.js
+++ understanding-typescript/app.js
@@ -2,21 +2,26 @@
 //     name: string;
 //     age: number;
 // } = {
-var person = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
-};
-person.role.push('admin');
-// person.role[1] = 10;
-var favoriteActivities; // can use any[], but that removes the benefits of typescript
-favoriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase()); // method available because hobby is a string type
-}
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]; // 'tuple' type -- fixed length array
+// } = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],  // hobbies is type string[]
+//     role: [2, 'author']
+// };
+// person.role.push('admin'); // 'push' is an exception and is allowed
+// person.role[1] = 10; // cannot change from string to number!
+// person.role = [0, 'admin', 'user']; // this is not allowed! we set the tuple length to 2!
+// let favoriteActivities: string[]; // can use any[], but that removes the benefits of typescript
+// favoriteActivities = ['Sports'];
+// // console.log(person.name);
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase()) // method available because hobby is a string type
+// }
 // ## # ## # ## # ## # ## # ## #
 // // js object
 // const product = {
@@ -38,3 +43,30 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
 // //       description: string;
 // //     }
 // // }
+// ENUM Type: enum {NEW, OLD}  Automatically enumerated global constant identifiers
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+; // Often, enums will be all-uppercase values but it's not a 'must-do'
+var person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+var favoriteActivities; // can use any[], but that removes the benefits of typescript
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase()); // method available because hobby is a string type
+}
+if (person.role === Role.AUTHOR) {
+    console.log('is read only');
+}
--- understanding-typescript/app.ts
+++ understanding-typescript/app.ts
@@ -2,33 +2,34 @@
 //     name: string;
 //     age: number;
 // } = {
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string]; // tuple type! 
-} = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],  // hobbies is type string[]
-    role: [2, 'author']
-};
 
-// person.role.push('admin');
-// person.role[1] = 10;
-
-// person.role = [0, 'admin', 'user'];
-
-let favoriteActivities: string[]; // can use any[], but that removes the benefits of typescript
-favoriteActivities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase()) // method available because hobby is a string type
-}
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]; // 'tuple' type -- fixed length array
+// } = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],  // hobbies is type string[]
+//     role: [2, 'author']
+// };
 
 
+// person.role.push('admin'); // 'push' is an exception and is allowed
+// person.role[1] = 10; // cannot change from string to number!
+
+// person.role = [0, 'admin', 'user']; // this is not allowed! we set the tuple length to 2!
+
+
+// let favoriteActivities: string[]; // can use any[], but that removes the benefits of typescript
+// favoriteActivities = ['Sports'];
+
+// // console.log(person.name);
+
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase()) // method available because hobby is a string type
+// }
 
 
 // ## # ## # ## # ## # ## # ## #
@@ -54,3 +55,35 @@ for (const hobby of person.hobbies) {
 // //       description: string;
 // //     }
 // // }
+
+
+
+// ENUM Type: enum {NEW, OLD}  Automatically enumerated global constant identifiers
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN, READ_ONLY, AUTHOR }; // Often, enums will be all-uppercase values but it's not a 'must-do'
+
+const person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],  // hobbies is type string[]
+    role: Role.ADMIN
+};
+
+
+let favoriteActivities: string[]; // can use any[], but that removes the benefits of typescript
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase()) // method available because hobby is a string type
+}
+
+
+if (person.role === Role.AUTHOR) { 
+    console.log('is read only');
+}
