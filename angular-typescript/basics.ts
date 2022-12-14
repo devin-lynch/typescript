536154d9ec66a1564b8f25fdb10a11cb50ce018d
devin-lynch <devin.l.lynch@gmail.com>
2022-12-14 14:53:39 -0600

    Base types

---
+++ angular-typescript/basics.ts
@@ -0,0 +1,40 @@
+// Primatives: numbers, strings, booleans
+// More complex types: arrays, objects
+// Function types, parameters
+
+
+
+// Primatives
+
+let age: number = 24; // can be a float as well
+age = 12; 
+
+let userName: string;
+userName = 'Max';
+
+let isInstructor: boolean;
+isInstructor = true;
+
+// let hobbies: null; // typically don't delcare a null type
+// hobbies = 12;
+
+
+
+// More complex types
+
+let hobbies: string[]; // an array of strings
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+    name: string,
+    age: number
+}; 
+person = {
+    name: 'Max',
+    age: 29
+};
+
+let people: {  // storing an array of objects!
+    name: string;
+    age: number;
+}[]; // notice the brackets after the object!
